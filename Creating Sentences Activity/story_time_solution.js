91cf9642554721d54e73d02662ba58559924ee7f
Sanjeevani26 <101032556+Sanjeevani26@users.noreply.github.com>
2022-07-06 12:15:46 +0530

    Add files via upload

---
+++ Creating Sentences Activity/story_time_solution.js
@@ -0,0 +1,28 @@
+// 4-Strings
+let greetings = "Welcome to my kingdom!";
+let name = 'Flare';
+let title = 'King of Dragons';
+let worldName = 'Warrior World'; 
+
+// 3-Integers
+let age = 2000;
+let year = 15000;
+let century = 150;
+
+// 1-Array
+let weaponsUsed = ['Sword','Wand','Ninja Stars', 'Dragon Fruit']; 
+
+// 1-Booleans
+let hasFavWeapon = true;
+
+console.log(greetings + " My name is " + name + ".");
+console.log("I'm the " + title);
+console.log("As the leader of " + worldName + ", I use different weapons.");
+console.log("Time works differently in my world.");
+console.log("For example, I'm " + age + " years old.");
+console.log("In my world, you are in year " + year + ", the " + century + "th century." + "\n");
+console.log("Does anyone have any questions?");
+console.log("Me: Do you have a favorite weapon?");
+console.log("Dragon: " + hasFavWeapon + ", my favorite weapon is " + weaponsUsed[3] + "." + "\n");
+console.log("You are free to explore my world and return to Earth after your tour is over!");
+
